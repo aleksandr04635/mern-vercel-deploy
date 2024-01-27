f37ff790f9d329694a61eef7cabad2dcd2bd9a7e
Oleksandr Liashchenko <lap04635@gmail.com>
2024-01-27 06:42:27 +0200

    v5

--- api/index.js
+++ api/index.js
@@ -4,9 +4,11 @@ const app = express();
 const cors = require("cors");
 app.use(cors({}));
 app.get("/api/test", function (req, res) {
-  res.status(200).send({ mes: "hello " + Date.now() });
+  res.status(200).send({ mes: "/api/test: " + Date.now() });
 });
-
+app.get("/test", function (req, res) {
+  res.status(200).send({ mes: "/test: " + Date.now() });
+});
 if (process.env.API_PORT) {
   app.listen({ port: process.env.API_PORT || 4500 }, () => {
     console.log(`Server OK on localhost:${process.env.API_PORT || 4500}`);
