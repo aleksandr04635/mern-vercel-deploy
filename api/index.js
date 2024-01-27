3d0f6d7f89b1b29fd3c41eedaf8a92cfca279e4a
Oleksandr Liashchenko <lap04635@gmail.com>
2024-01-27 07:38:22 +0200

    v13

--- api/index.js
+++ api/index.js
@@ -3,19 +3,24 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors({}));
+
 app.get("/api/test", function (req, res) {
   console.log("/api/test");
   res.json({ mes: "/api/test: " + Date.now() });
 });
+
 app.get("/test", function (req, res) {
   console.log("/test");
   res.json({ mes: "/test: " + Date.now() });
 });
-if (process.env.API_PORT) {
+/* if (process.env.API_PORT) {
   app.listen({ port: process.env.API_PORT || 4500 }, () => {
     console.log(`Server OK on localhost:${process.env.API_PORT || 4500}`);
   });
-}
+} */
+app.listen({ port: process.env.API_PORT || 4500 }, () => {
+  console.log(`Server OK on localhost:${process.env.API_PORT || 4500}`);
+});
 /* app.listen(4000, (err) => {
   if (err) {
     return console.log(err);
